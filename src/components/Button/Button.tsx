1b189844a3cdb0fd52f2da32aff45f4cc9ce1f62
Hoang Ha <hha@opentext.com>
2023-02-02 22:22:21 -0500

    test commit with husky

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -23,12 +23,11 @@ export const Button = ({
   const mode = primary
     ? "storybook-button--primary"
     : "storybook-button--secondary";
-
   return (
     <button
       type="button"
       className={["storybook-button", `storybook-button--${size}`, mode].join(
-        " "
+        " ",
       )}
       style={backgroundColor && { backgroundColor }}
       {...props}
